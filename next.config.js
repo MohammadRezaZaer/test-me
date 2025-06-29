fdfc9068b32142c64071119053909a5640a31d73
mrzzrm <mrzzrm27@gmail.com>
2025-06-30 03:23:42 +0330

    ignoreDuringBuilds

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
   eslint: {
     dirs: ['src'],
+    ignoreDuringBuilds: true,
   },
-
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
 
